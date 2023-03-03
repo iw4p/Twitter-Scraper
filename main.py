2b1dcb4a47d46ec6fe3984c1b85defc229384f01
Nima Akbarzade <Nimakbarzade@yahoo.com>
2023-03-03 02:29:02 +0100

    add: replies API

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 from fastapi import FastAPI
 from scrape_profile_tweets import scrape_profile_tweets_since_2023
+from scrape_replies import scrape_replies
 
 app = FastAPI()
 
+# - /accounts: return a json list of all tracked accounts.
+@app.get("/accounts/")
+async def replies():
+    res = {'users': ['alikarimi_ak8', 'elonmusk', 'BarackObama', 'taylorlorenz', 'cathiedwood', 'ylecun']}
+    return res
+
+# - /tweets/<twitter-handle> : return a json of the user's conversation threads since start.
 @app.get("/tweets/{twitter_handle}")
 async def tweets(twitter_handle):
     res = scrape_profile_tweets_since_2023(twitter_handle)
+    return res
+
+@app.get("/replies/{twitter_handle}")
+async def replies(twitter_handle):
+    res = scrape_replies(twitter_handle)
+    return res
+    # https://twitter.com/BrianMteleSUR/status/1625111883626823681
+
+# - /audience/<twitter-handle> : return a json of information about the audience for a user's account.
+@app.get("/audience/{twitter_handle}")
+async def audience(twitter_handle):
+    res = x(twitter_handle)
+    return res
+
+# - /sentiment/<twitter-handle> : return a json about the sentiment information of an account (e.g. thread level, audience level)
+@app.get("/sentiment/{twitter_handle}")
+async def sentiment(twitter_handle):
+    res = x(twitter_handle)
     return res
--- scrape_replies.py
+++ scrape_replies.py
@@ -18,138 +18,135 @@ class Tweet:
     replying_to: str
     text: str
 
-target_tweet='https://twitter.com/BrianMteleSUR/status/1625111883626823681'
-driver = webdriver.Chrome()
-tweets = []
+def scrape_replies(target_tweet: str):
+    # target_tweet='https://twitter.com/BrianMteleSUR/status/1625111883626823681'
+    driver = webdriver.Chrome()
+    tweets = []
 
-driver.get(target_tweet)
-sleep(6)
+    driver.get(target_tweet)
+    sleep(6)
 
-MAX_SCROLLS=5
-for _ in range(MAX_SCROLLS):
-    last = driver.find_elements(By.XPATH, '//div[@data-testid="cellInnerDiv"]')[-1]
-    driver.execute_script("arguments[0].scrollIntoView(true)", last)
-    time.sleep(.2)
-    all_tweets = driver.find_elements(By.XPATH, '//div[@data-testid]//article[@data-testid="tweet"]')
-    for item in all_tweets[1:]:
- 
-            try:
-                date = item.find_element(By.XPATH, './/time').text
-            except:
-                date = '[empty]'
+    MAX_SCROLLS=5
+    for _ in range(MAX_SCROLLS):
+        last = driver.find_elements(By.XPATH, '//div[@data-testid="cellInnerDiv"]')[-1]
+        driver.execute_script("arguments[0].scrollIntoView(true)", last)
+        time.sleep(.2)
+        all_tweets = driver.find_elements(By.XPATH, '//div[@data-testid]//article[@data-testid="tweet"]')
+        for item in all_tweets[1:]:
+    
+                try:
+                    date = item.find_element(By.XPATH, './/time').text
+                except:
+                    date = '[empty]'
 
-            try:
-                text = item.find_element(By.XPATH, './/div[@data-testid="tweetText"]').text
-            except:
-                text = '[empty]'
+                try:
+                    text = item.find_element(By.XPATH, './/div[@data-testid="tweetText"]').text
+                except:
+                    text = '[empty]'
 
-            try:
-                author_name_handle = item.find_element(By.XPATH, './/div[@data-testid="User-Names"]/div[2]/div/div/a/div/span').text
-            except:
-                author_name_handle = '[empty]'
+                try:
+                    author_name_handle = item.find_element(By.XPATH, './/div[@data-testid="User-Names"]/div[2]/div/div/a/div/span').text
+                except:
+                    author_name_handle = '[empty]'
 
-            try:
-                author_id_handle = item.find_element(By.XPATH, './/div[@data-testid="User-Names"]//div//span//span').text
-            except:
-                author_id_handle = '[empty]'
+                try:
+                    author_id_handle = item.find_element(By.XPATH, './/div[@data-testid="User-Names"]//div//span//span').text
+                except:
+                    author_id_handle = '[empty]'
 
-            try:
-                replying_to = item.find_element(By.XPATH, './/div[contains(text(), "Replying to")]//a').text
-            except:
-                replying_to = '[empty]'
-            
-            curr_tweet = Tweet(
-                date=date,
-                author_name_handle=author_name_handle,
-                author_id_handle=author_id_handle,
-                replying_to=replying_to,
-                text=text,
-            )
+                try:
+                    replying_to = item.find_element(By.XPATH, './/div[contains(text(), "Replying to")]//a').text
+                except:
+                    replying_to = '[empty]'
+                
+                curr_tweet = Tweet(
+                    date=date,
+                    author_name_handle=author_name_handle,
+                    author_id_handle=author_id_handle,
+                    replying_to=replying_to,
+                    text=text,
+                )
 
-            if curr_tweet is not None:
-                tweets.append(curr_tweet)
+                if curr_tweet is not None:
+                    tweets.append(curr_tweet)
 
-            time.sleep(.2)
-            
-print(f'Found {len(tweets)} replies.')
+                time.sleep(.2)
+                
+    print(f'Found {len(tweets)} replies.')
 
-# Scraping the replies loaded by the 'load more replies' button, if there are such. 
-#The "load more replies" XPATH changes dinamically and i cannot figure out the mechanics, so for now i'm brute-forcing it.
-# Filling the missing values with None 
+    # Scraping the replies loaded by the 'load more replies' button, if there are such. 
+    #The "load more replies" XPATH changes dinamically and i cannot figure out the mechanics, so for now i'm brute-forcing it.
+    # Filling the missing values with None 
 
-for i in range(20):
-    try:
-        show_more=driver.find_element(By.XPATH, value=f'/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/section/div/div/div[{i}]/div/div/div/div/div/span')
-        show_more.click()
-        print('Found more replies!')
-        for _ in range(MAX_SCROLLS):
-            last = driver.find_elements(By.XPATH, '//div[@data-testid="cellInnerDiv"]')[-1]
-            driver.execute_script("arguments[0].scrollIntoView(true)", last)
-            time.sleep(.2)
-            all_tweets = driver.find_elements(By.XPATH, '//div[@data-testid]//article[@data-testid="tweet"]')
-            for item in all_tweets:
+    for i in range(20):
+        try:
+            show_more=driver.find_element(By.XPATH, value=f'/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/section/div/div/div[{i}]/div/div/div/div/div/span')
+            show_more.click()
+            print('Found more replies!')
+            for _ in range(MAX_SCROLLS):
+                last = driver.find_elements(By.XPATH, '//div[@data-testid="cellInnerDiv"]')[-1]
+                driver.execute_script("arguments[0].scrollIntoView(true)", last)
+                time.sleep(.2)
+                all_tweets = driver.find_elements(By.XPATH, '//div[@data-testid]//article[@data-testid="tweet"]')
+                for item in all_tweets:
 
-                    try:
-                        date = item.find_element(By.XPATH, './/time').text
-                    except:
-                        date = None
+                        try:
+                            date = item.find_element(By.XPATH, './/time').text
+                        except:
+                            date = None
 
-                    try:
-                        text = item.find_element(By.XPATH, './/div[@data-testid="tweetText"]').text
-                    except:
-                        text = None
+                        try:
+                            text = item.find_element(By.XPATH, './/div[@data-testid="tweetText"]').text
+                        except:
+                            text = None
 
-                    try:
-                        author_name_handle = item.find_element(By.XPATH, './/div[@data-testid="User-Names"]/div[2]/div/div/a/div/span').text
-                    except:
-                        author_name_handle = '[empty]'
+                        try:
+                            author_name_handle = item.find_element(By.XPATH, './/div[@data-testid="User-Names"]/div[2]/div/div/a/div/span').text
+                        except:
+                            author_name_handle = '[empty]'
 
-                    try:
-                        author_id_handle = item.find_element(By.XPATH, './/div[@data-testid="User-Names"]//div//span//span').text
-                    except:
-                        author_id_handle = '[empty]'
+                        try:
+                            author_id_handle = item.find_element(By.XPATH, './/div[@data-testid="User-Names"]//div//span//span').text
+                        except:
+                            author_id_handle = '[empty]'
 
-                    try:
-                        replying_to = item.find_element(By.XPATH, './/div[contains(text(), "Replying to")]//a').text
-                    except:
-                        replying_to = None
+                        try:
+                            replying_to = item.find_element(By.XPATH, './/div[contains(text(), "Replying to")]//a').text
+                        except:
+                            replying_to = None
 
-                    curr_tweet = Tweet(
-                        date=date,
-                        author_name_handle=author_name_handle,
-                        author_id_handle=author_id_handle,
-                        replying_to=replying_to,
-                        text=text,
-                    )
-                    if curr_tweet is not None:
-                        tweets.append(curr_tweet)
+                        curr_tweet = Tweet(
+                            date=date,
+                            author_name_handle=author_name_handle,
+                            author_id_handle=author_id_handle,
+                            replying_to=replying_to,
+                            text=text,
+                        )
+                        if curr_tweet is not None:
+                            tweets.append(curr_tweet)
 
-                    time.sleep(.2)
+                        time.sleep(.2)
 
-        print(f'Found {len(tweets)} replies totally.')
-        driver.quit()
-    except:
-        continue
+            print(f'Found {len(tweets)} replies totally.')
+            driver.quit()
+        except:
+            continue
 
-print('tweets')
-print(tweets)
-outdict = [d.__dict__ for d in tweets]
-print('outdict')
-print(outdict)
-unique_tweets = list({each["text"]: each for each in outdict}.values())
+    outdict = [d.__dict__ for d in tweets]
+    unique_tweets = list({each["text"]: each for each in outdict}.values())
 
-for d in unique_tweets:
-    d["author_name_handle"] = d["author_name_handle"]
-    d["date"] = d["date"]
-    d["text"] = d["text"]
-    d["author_id_handle"] = re.sub("[/@]", "", d["author_id_handle"])
-    d["replying_to"] = d["replying_to"]
-    # d["replying_to"] = d["replying_to"].replace("@" + d["author_name_handle"], "")
+    for d in unique_tweets:
+        d["author_name_handle"] = d["author_name_handle"]
+        d["date"] = d["date"]
+        d["text"] = d["text"]
+        d["author_id_handle"] = re.sub("[/@]", "", d["author_id_handle"])
+        d["replying_to"] = d["replying_to"]
+        # d["replying_to"] = d["replying_to"].replace("@" + d["author_name_handle"], "")
 
-print(json.dumps(unique_tweets, indent=4, ensure_ascii=False))
-print(f"FOUND {len(unique_tweets)} TWEETS")
+    result = (json.dumps(unique_tweets, indent=4, ensure_ascii=False))
+    # print(f"FOUND {len(unique_tweets)} TWEETS")
 
-output_file = 'result.json'
+    # output_file = 'result.json'
 
-with open(output_file, "w", encoding="utf-8") as f:
-    json.dump(unique_tweets, f, ensure_ascii=False, indent=4)
+    # with open(output_file, "w", encoding="utf-8") as f:
+    #     json.dump(unique_tweets, f, ensure_ascii=False, indent=4)
